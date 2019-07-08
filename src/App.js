e72e1cc50e7cc91846e0fe583f13655e17e4c743
ginnnnnn <aginlo@hotmail.com>
2019-07-08 16:45:43 +0800

    add shop page preview'

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
+
 
 
 function App() {
@@ -18,7 +13,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
